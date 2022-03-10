f17bcbd0dcecfb0388e07095dd34b436e49abc08
SuruXmanager <100470320+SuruXmanager@users.noreply.github.com>
2022-03-10 19:46:43 +0530

    Create games.py

---
+++ Teddybot/modules/games.py
@@ -0,0 +1,57 @@
+import random
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+import Teddybot.modules.game_strings as game_strings
+from Teddybot import dispatcher
+from Teddybot.modules.disable import DisableAbleCommandHandler
+
+
+def truth(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(random.choice(game_strings.TRUTH_STRINGS))
+
+
+def dare(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(random.choice(game_strings.DARE_STRINGS))
+
+
+def tord(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(random.choice(game_strings.TORD_STRINGS))
+
+
+def wyr(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(random.choice(game_strings.WYR_STRINGS))
+
+
+__help__ = """
+ • `/truth`*:* asks you a question
+ • `/dare`*:* gives you a dare
+ • `/tord`*:* can be a truth or a dare
+ • `/rather`*:* would you rather
+  """
+
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
+DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
+TORD_HANDLER = DisableAbleCommandHandler("tord", tord, run_async=True)
+WYR_HANDLER = DisableAbleCommandHandler("rather", wyr, run_async=True)
+
+dispatcher.add_handler(TRUTH_HANDLER)
+dispatcher.add_handler(DARE_HANDLER)
+dispatcher.add_handler(TORD_HANDLER)
+dispatcher.add_handler(WYR_HANDLER)
+
+__mod_name__ = "Games"
+__command_list__ = [
+    "truth",
+    "dare",
+    "tord",
+    "rather",
+]
+
+__handlers__ = [
+    TRUTH_HANDLER,
+    DARE_HANDLER,
+    TORD_HANDLER,
+    WYR_HANDLER,
+]
